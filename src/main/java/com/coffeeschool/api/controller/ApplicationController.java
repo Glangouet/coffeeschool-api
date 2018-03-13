c4394a28c4c889fa485ad18906b5225e98649795
Ludo Renzetti <ludovic.renzetti@gmail.com>
2018-03-13 11:52:04 +0100

    Reset project with Spring boot
    Using web services Spring boot sample

---
+++ src/main/java/com/coffeeschool/api/controller/ApplicationController.java
@@ -0,0 +1,39 @@
+package com.coffeeschool.api.controller;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ApplicationController {
+
+    public class Greeting {
+        private final long id;
+        private final String content;
+
+        public Greeting(long id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public String getContent() {
+            return content;
+        }
+    }
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @ResponseBody
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
+    public Greeting sayHello(@RequestParam(name="name", required=false, defaultValue="Stranger") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
